25e2da738be1c519ab02a87bb05c3255d3f8c56b
Arsenic <81708177+ArsenicBite@users.noreply.github.com>
2024-11-11 12:27:01 +0000

    Changes

---
+++ music-assaignment/main_game.gd
@@ -0,0 +1,37 @@
+extends Node2D
+#1
+func _on_1_pressed() -> void:
+	$"(1)".play()
+#2
+func _on_2_pressed() -> void:
+	$"(2)".play()
+#3
+func _on_3_pressed() -> void:
+	$"(3)".play()
+#4
+func _on_4_pressed() -> void:
+	$"(4)".play()
+
+func _on_5_pressed() -> void:
+	$"(5)".play()
+
+func _on_6_pressed() -> void:
+	$"(6)".play()
+	
+func _on_7_pressed() -> void:
+	$"(7)".play()
+
+func _on_8_pressed() -> void:
+	$"(8)".play()
+
+func _on_9_pressed() -> void:
+	$"(9)".play()
+
+func _on_10_pressed() -> void:
+	$"(10)".play()
+
+func _on_11_pressed() -> void:
+	$"11".play()
+
+func _on_12_pressed() -> void:
+	$"(12)".play()
